e2d2dfff3c4c9bd7e8f1d556f47244c7650df83d
julia98 <julia.shettler98@gmail.com>
2015-08-11 14:38:31 -0700

    uh change fix stuf sick

--- Jam.js
+++ Jam.js
@@ -1,14 +1,16 @@
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function showInfo(data, tabletop) {
-    alert("Successfully processed!")
-    console.log(data);
+    console.log(data.moderate.elements[getRandomIntInclusive(0,8)].moderate);
   }
 
+var tabletop;
+
 function init() {
-var tabletop = Tabletop.init( { key: '1HOgOyuzD_NILAxZ4iAgfipaJofB-hRL2b7nabg8b6GY',
-                   callback: function(data, tabletop) { console.log(data) },
-                   simpleSheet: true } );
-				var sheet = tabletop.sheets("$");
-				console.log(sheet);
+tabletop = Tabletop.init( { key: '1HOgOyuzD_NILAxZ4iAgfipaJofB-hRL2b7nabg8b6GY',
+                   callback: showInfo,} );
 }
 
 window.onload = init();
