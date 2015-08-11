7a0458b2d38e7ea582505cf3b398035f7f3a0435
julia98 <julia.shettler98@gmail.com>
2015-08-11 11:12:53 -0700

    Create tabletop

---
+++ Jam.js
@@ -0,0 +1,17 @@
+alert("Hello world");
+
+function showInfo(data, tabletop) {
+    alert("Successfully processed!")
+    console.log(data);
+  }
+
+function init() {
+var tabletop = Tabletop.init( { key: '1HOgOyuzD_NILAxZ4iAgfipaJofB-hRL2b7nabg8b6GY',
+                   callback: function(data, tabletop) { console.log(data) },
+                   simpleSheet: true } );
+				var sheet = tabletop.sheets("$");
+				console.log(sheet);
+}
+
+window.onload = init();
+
