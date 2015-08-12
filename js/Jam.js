499d17de7707d1ea87c74d9b32c2af4373796fb1
haleylam78 <haleylam98@yahoo.com>
2015-08-12 14:51:26 -0700

    fixed random index 2.0

--- js/Jam.js
+++ js/Jam.js
@@ -28,8 +28,8 @@ function getRandomIntInclusive(min, max) {
 //Show a random index
 function showInfo(data, tabletop) { (data.cheap.elements[getRandomIntInclusive(0,8)].cheap);
 	moderate = (data.moderate.elements[getRandomIntInclusive(0,8)].moderate);
-	pricey = (data.pricey.elements[getRandomIntInclusive(0,4)].pricey);
-	expensive = (data.expensive.elements[getRandomIntInclusive(0,2)].expensive);
+	pricey = (data.pricey.elements[getRandomIntInclusive(0,10)].pricey);
+	expensive = (data.expensive.elements[getRandomIntInclusive(0,10)].expensive);
 }
 
 window.onload = init();
