4205c0a308421ea866d34ac666d83e52b7080d0a
julia98 <julia.shettler98@gmail.com>
2015-08-13 15:47:19 -0700

    fixed save list

--- js/Jam.js
+++ js/Jam.js
@@ -13,8 +13,15 @@ var expensiveCalled = false;
 *Initialize the spreadsheet
 */
 function init() {
-    Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1HOgOyuzD_NILAxZ4iAgfipaJofB-hRL2b7nabg8b6GY/pubhtml',
-                     callback: showInfo,} )
+	Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1HOgOyuzD_NILAxZ4iAgfipaJofB-hRL2b7nabg8b6GY/pubhtml',
+					 callback: showInfo,} )
+	
+}
+
+function init2(){
+	Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1HOgOyuzD_NILAxZ4iAgfipaJofB-hRL2b7nabg8b6GY/pubhtml',
+					 callback: getData,} )
+	
 }
 
 /*
@@ -27,46 +34,88 @@ function getRandomIntInclusive(min, max) {
 
 //Show a random index
 
+var cheapList = [];
+var moderateList = [];
+var priceyList = [];
+var expensiveList = [];
+function getData(data, tabletop){
+for (i = 0; i<16; i++){
+		cheapList.push(data.cheap.elements[i].cheap);
+	}
+	console.log(cheapList);
+	for (i = 0; i<15; i++){
+		moderateList.push(data.moderate.elements[i].moderate);	
+	}
+	console.log(moderateList);
+	for (i = 0; i<9; i++){
+		priceyList.push(data.pricey.elements[i].pricey);
+	}
+		console.log(priceyList);
+	for (i = 0; i<13; i++){
+		expensiveList.push(data.expensive.elements[i].expensive);	
+	}
+	console.log(expensiveList);
+}
 function showInfo(data, tabletop) { 
-	cheap = (data.cheap.elements[getRandomIntInclusive(0,8)].cheap);
-	moderate = (data.moderate.elements[getRandomIntInclusive(0,8)].moderate);
-	pricey = (data.pricey.elements[getRandomIntInclusive(0,10)].pricey);
-	expensive = (data.expensive.elements[getRandomIntInclusive(0,2)].expensive);
+	cheap = (data.cheap.elements[getRandomIntInclusive(0,16)].cheap);
+	moderate = (data.moderate.elements[getRandomIntInclusive(0,15)].moderate);
+	pricey = (data.pricey.elements[getRandomIntInclusive(0,9)].pricey);
+	expensive = (data.expensive.elements[getRandomIntInclusive(0,13)].expensive);
+}
+
+function resetButton(){
+	for (i = 0; i<cheapList.length-1;i++){
+		cheap = cheapList[getRandomIntInclusive(0,16)];
+	}
+	console.log(cheap);
+	for (i = 0; i<moderateList.length-1; i++){
+		moderate = moderateList[getRandomIntInclusive(0,15)];
+	}
+	console.log(moderate);
+	for (i = 0; i<priceyList.length-1; i++){
+		pricey = priceyList[getRandomIntInclusive(0,9)];
+	}
+	console.log(pricey);
+	for (i = 0; i<expensiveList.length-1; i++){
+		expensive = expensiveList[getRandomIntInclusive(0,13)];
+	}
+	console.log(expensive);
 }
 
 window.onload = init();
+window.onload = init2();
 
 //Display when clicked
 function oneDollar () {
-    document.getElementById("changeText").innerHTML = cheap;
-    moderateCalled = false;
-    priceyCalled = false;
-    expensiveCalled = false; 
-    cheapCalled = true;
+	document.getElementById("changeText").innerHTML = cheap;
+	moderateCalled = false;
+	priceyCalled = false;
+	expensiveCalled = false; 
+	cheapCalled = true;
 }
 
 function twoDollar () {
-    document.getElementById("changeText").innerHTML = moderate;
-    cheapCalled = false;
-    priceyCalled = false;
-    expensiveCalled = false;
-    moderateCalled = true;
+	document.getElementById("changeText").innerHTML = moderate;
+	cheapCalled = false;
+	priceyCalled = false;
+	expensiveCalled = false;
+	moderateCalled = true;
 }
 
 function threeDollar () {
-    document.getElementById("changeText").innerHTML = pricey;
-    cheapCalled = false;
-    moderateCalled = false;
-    expensiveCalled = false;
-    priceyCalled = true;
+	document.getElementById("changeText").innerHTML = pricey;
+	cheapCalled = false;
+	moderateCalled = false;
+	expensiveCalled = false;
+	priceyCalled = true;
 }
 
 function fourDollar () {
-    document.getElementById("changeText").innerHTML = expensive;
-    cheapCalled = false;
-    moderateCalled = false;
-    priceyCalled = false;
-    expensiveCalled = true;
+	document.getElementById("changeText").innerHTML = expensive;
+	cheapCalled = false;
+	moderateCalled = false;
+	priceyCalled = false;
+	expensiveCalled = true;
 }
 
 var savedIdeas = [];
